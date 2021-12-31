1fa756f3cf26ce67f5612cd42846a4c78609f749
[REDACTED] <54645548+StrongSpider@users.noreply.github.com>
2021-12-31 00:56:33 -0500

    Polished the code a bit.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, Intents, User } = require('discord.js');
+const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MEMBERS] });
 const config = require('./config.json')
 const axios = require('axios')
@@ -13,89 +13,75 @@ app.get("/", function (req, res) {
 })
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
 
-function sendWebhook(targetName){
-    var params = {
-        embeds: [
-            {
-                "color": 16711680,
-                "fields": [
-                    {
-                        "name": "User Banned from Server",
-                        "value": `${targetName} has been banned banned by this bot! 🎉🎉🎉\n#MakeDiscordGreatAgain`
-                    }
-                ]
-            }
-        ]
-    }
-
-    axios.post('https://canary.discord.com/api/webhooks/926222398099951626/Dw2yTHZ_iWDVR8rGw9BsUcM6qzw4N3iGjFuXie-zM6Sv7QBDEeh4unzK9_ZDwb0rcctg', params).catch(console.log)
+//Logging Embed parameters
+var loggingParams = {
+    embeds: [
+        {
+            "color": 16711680,
+            "fields": [
+                {
+                    "name": "User Banned from Server",
+                    "value": `${targetName} has been banned banned by this bot! 🎉🎉🎉\n#MakeDiscordGreatAgain`
+                }
+            ]
+        }
+    ]
 }
 
-function checkUserBadge(user){
-    if(user.flags !== null) {
-    const flags = user.flags.serialize();
-      for (const [key, value] of Object.entries(flags)) {
-         for (const badge of config.tags){
-             if (key === badge){
-                 if(value){
-                        return true;
+function checkUserBadge(GuildMember) {
+    let user = GuildMember.user
+
+    //Flags = Badges
+    //Checks if user has no badges
+    if (user.flags !== null) {
+        const flags = user.flags.serialize();
+        for (const [key, value] of Object.entries(flags)) {
+            for (const badge of config.tags) {
+                //Checks if user badge is on the naughty list
+                if (key === badge) {
+                    if (value) {
+                        //Bans user
+                        GuildMember.ban({ reason: "User has blocked badge." })
+                            .then(() => {
+                                //Log Ban in console for debugging
+                                console.log(user.username + " has been banned.")
+
+                                //Informs the server owner of banning
+                                GuildMember.guild.fetchOwner().then(owner => {
+                                    owner.createDM().then(channel => channel.send(user.username + " has been banned for having a restricted badge."))
+                                })
+
+                                //Log Bans in main Discord
+                                axios.post(config.loggingWebhook, loggingParams).catch(console.log)
+                            })
+                        .catch(console.log)
                     }
-             }
-          }
-      }
+                }
+            }
+        }
     }
-    return false;
 }
 
 client.on('ready', () => {
+    //Sets presence
     console.log(`Logged in as ${client.user.tag}!`);
     //client.user.bannerURL('https://i.ibb.co/R4NsnM4/Bot-Banner.png')
-    client.user.setPresence({ activities: [{ name: 'Making Discord Great Again' }]})
-    client.guilds.cache.forEach( Guild => {
-        Guild.members.cache.forEach( GuildMember => {
-            if (checkUserBadge(GuildMember.user)){
-                GuildMember.ban({reason: "User has blocked badge."})
-                .then( () => {
-                    console.log(GuildMember.user.username + " has been banned.")
-                    GuildMember.guild.fetchOwner().then(owner => {
-                        owner.createDM().then( channel => channel.send(GuildMember.user.username + " has been banned for having a restricted badge."))
-                    })
-                    sendWebhook(GuildMember.user.username)
-                })
-                .catch(console.log)
-            }
-         });
+    client.user.setPresence({ activities: [{ name: 'Making Discord Great Again' }] })
+
+    //Checks all guilds for any badge updates when bot was offline
+    client.guilds.cache.forEach(Guild => {
+        Guild.members.cache.forEach(checkUserBadge);
     });
 });
 
 client.on('guildMemberAdd', GuildMember => {
-    if (checkUserBadge(GuildMember.user)){
-        GuildMember.ban({reason: "User has blocked badge."})
-        .then( () => {
-            console.log(GuildMember.user.username + " has been banned.")
-            GuildMember.guild.fetchOwner().then(owner => {
-                owner.createDM().then( channel => channel.send(GuildMember.user.username + " has been banned for having a restricted badge."))
-            })
-            sendWebhook(GuildMember.user.username)
-        })
-        .catch(console.log)
-    }
+    //Checks badge when join server
+    checkUserBadge(GuildMember);
 });
 
 client.on('guildCreate', async Guild => {
-     Guild.members.cache.forEach( GuildMember => {
-        if (checkUserBadge(GuildMember.user)){
-            GuildMember.ban({reason: "User has blocked badge."})
-            .then( () => {
-                console.log(GuildMember.user.username + " has been banned.")
-                GuildMember.guild.fetchOwner().then(owner => {
-                    owner.createDM().then( channel => channel.send(GuildMember.user.username + " has been banned for having a restricted badge."))
-                })
-                sendWebhook(GuildMember.user.username)
-            })
-            .catch(console.log)
-        }
-     })
- });
+    //Checks all members badge when bot joins server
+    Guild.members.cache.forEach(checkUserBadge);
+});
 
 client.login(config.token);
